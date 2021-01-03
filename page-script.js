b90784a4f90f2cdffba7dec0361705ddca0d2e09
Alec Ahlbrandt <alec@ahlbrandt.net>
2021-01-02 22:45:48 -0600

    add some logging

--- page-script.js
+++ page-script.js
@@ -1,14 +1,25 @@
-const endpointUrl = 'https://bigurl.herokuapp.com/';
-const hrefs = document.getElementsByTagName('a');
-for (let i = 0; i < hrefs.length; i++) {
-  const x = hrefs[i];
-  const url = `${endpointUrl}${encodeURIComponent(x.href)}`;
-  fetch(url).then(res => res.json()).then(json => {
-    if(x.href != json.url) {
+const main = async () => {
+  const endpointUrl = 'https://bigurl.herokuapp.com/';
+  const hrefs = document.getElementsByTagName('a');
+  let bogus = 0;
+  for (let i = 0; i < hrefs.length; i++) {
+    const href = hrefs[i].href;
+    console.log('evaluating link', href, i, 'of', hrefs.length);
+    const url = `${endpointUrl}${encodeURIComponent(href)}`;
+    const json = await (await fetch(url)).json();
+    if (href != json.url) {
       console.log('FOUND DIFFERENT URL!!!!!!!!');
-      console.log('source:', x.href);
+      console.log('source:', href);
       console.log('destination:', json.url);
-      x.href = json.url
+      href = json.url
+      bogus++;
     }
-  });
-}
+  }
+  console.log(`of the ${hrefs.length} urls found, ${bogus} were bogus (not authentic).`);
+  const caution = bogus / hrefs.length >= .5
+  if (caution) {
+    console.log(`${caution * 100}% of the urls on this page could be trackers. Use with care.`);
+  }
+};
+
+main();
