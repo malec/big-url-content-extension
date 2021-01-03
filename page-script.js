b96d0e0a4f407fc7cbb4a19b7b77497c643bd74d
Alec Ahlbrandt <alec@ahlbrandt.net>
2021-01-03 08:45:42 -0600

    add logging

--- page-script.js
+++ page-script.js
@@ -2,17 +2,22 @@ const main = async () => {
   const endpointUrl = 'https://bigurl.herokuapp.com/';
   const hrefs = document.getElementsByTagName('a');
   let bogus = 0;
+  console.log(hrefs.length, 'links to eval');
   for (let i = 0; i < hrefs.length; i++) {
+    console.log('i', i, 'l', hrefs.length);
     const href = hrefs[i].href;
     console.log('evaluating link', href, i, 'of', hrefs.length);
     const url = `${endpointUrl}${encodeURIComponent(href)}`;
-    const json = await (await fetch(url)).json();
-    if (href != json.url) {
-      console.log('FOUND DIFFERENT URL!!!!!!!!');
-      console.log('source:', href);
-      console.log('destination:', json.url);
-      href = json.url
-      bogus++;
+    if (url != 'javascript:void(0);') {
+      const json = await (await fetch(url)).json();
+      if (href != json.url) {
+        console.log('FOUND DIFFERENT URL!!!!!!!!');
+        console.log('source:', href);
+        console.log('destination:', json.url);
+        hrefs[i].href = json.url
+        bogus++;
+      }
+      console.log('done');
     }
   }
   console.log(`of the ${hrefs.length} urls found, ${bogus} were bogus (not authentic).`);
